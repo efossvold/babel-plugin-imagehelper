28574ad1b71756dd082885ea20a423a5b500a653
Erik Fossvold <erik.fossvold@fanbooster.com>
2018-05-26 11:29:09 +0200

    Added md5sum and base64 data

--- __tests__/index.js
+++ __tests__/index.js
@@ -13,41 +13,50 @@ pluginTester({
       title: 'Transform',
       code,
       output: `
-export default {
-  'img.jpg': {
-    path: 'img/img.jpg',
-    width: 1200,
-    widthPx: '1200px',
-    height: 630,
-    heightPx: '630px',
-    aspectRatio: 0.525
-  },
-  'img.png': {
-    path: 'img/img.png',
-    width: 455,
-    widthPx: '455px',
-    height: 454,
-    heightPx: '454px',
-    aspectRatio: 0.9978021978021978
-  },
-  'img.svg': {
-    path: 'img/img.svg',
-    width: 580,
-    widthPx: '580px',
-    height: 580,
-    heightPx: '580px',
-    aspectRatio: 1
-  },
-  'subdir/img.jpg': {
-    path: 'img/subdir/img.jpg',
-    width: 1200,
-    widthPx: '1200px',
-    height: 630,
-    heightPx: '630px',
-    aspectRatio: 0.525
-  }
-};
-`
+      export default {
+        'img.jpg': {
+          path: 'img/img.jpg',
+          ext: '.jpg',
+          width: 1200,
+          widthPx: '1200px',
+          height: 630,
+          heightPx: '630px',
+          aspectRatio: 0.525,
+          hash: '741d27da'
+        },
+        'img.png': {
+          path: 'img/img.png',
+          ext: '.png',
+          width: 455,
+          widthPx: '455px',
+          height: 454,
+          heightPx: '454px',
+          aspectRatio: 0.9978021978021978,
+          hash: '80050422'
+        },
+        'img.svg': {
+          path: 'img/img.svg',
+          ext: '.svg',
+          width: 580,
+          widthPx: '580px',
+          height: 580,
+          heightPx: '580px',
+          aspectRatio: 1,
+          hash: 'ecb357cd',
+          data: 'PHN2ZyB2aWV3Qm94PSIwIDAgNTgwIDU4MCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayI+CiAgPGRlZnM+CiAgICA8cmVjdCBpZD0iYSIgd2lkdGg9IjU4MCIgaGVpZ2h0PSI1ODAiIHJ4PSIyOTAiLz4KICA8L2RlZnM+CiAgPGcgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj4KICAgIDxtYXNrIGlkPSJiIiBmaWxsPSIjZmZmIj4KICAgICAgPHVzZSB4bGluazpocmVmPSIjYSIvPgogICAgPC9tYXNrPgogICAgPHVzZSBmaWxsPSIjNEU2RjgyIiB4bGluazpocmVmPSIjYSIvPgogICAgPHJlY3QgZmlsbD0iI0QyREZFNyIgbWFzaz0idXJsKCNiKSIgdHJhbnNmb3JtPSJyb3RhdGUoMTAgMzIzIDQwMS41KSIgeD0iMTczIiB5PSIxNjciIHdpZHRoPSIzMDAiIGhlaWdodD0iNDY5IiByeD0iMTAiLz4KICAgIDxyZWN0IGZpbGw9IiNGRkYiIG1hc2s9InVybCgjYikiIHg9IjE0MCIgeT0iMTQzIiB3aWR0aD0iMzAwIiBoZWlnaHQ9IjQ2OSIgcng9IjEwIi8+CiAgICA8cmVjdCBmaWxsPSIjNEU2RjgyIiBtYXNrPSJ1cmwoI2IpIiB4PSIxODAiIHk9IjQzMCIgd2lkdGg9IjIyMCIgaGVpZ2h0PSIxMCIgcng9IjUiLz4KICAgIDxyZWN0IGZpbGw9IiM0RTZGODIiIG1hc2s9InVybCgjYikiIHg9IjE4MCIgeT0iNDU4IiB3aWR0aD0iMjA3IiBoZWlnaHQ9IjYiIHJ4PSIzIi8+CiAgICA8cmVjdCBmaWxsPSIjNEU2RjgyIiBtYXNrPSJ1cmwoI2IpIiB4PSIxODAiIHk9IjQ3NCIgd2lkdGg9IjIxNCIgaGVpZ2h0PSI2IiByeD0iMyIvPgogICAgPHJlY3QgZmlsbD0iIzRFNkY4MiIgbWFzaz0idXJsKCNiKSIgeD0iMTgwIiB5PSI0OTAiIHdpZHRoPSIyMDkiIGhlaWdodD0iNiIgcng9IjMiLz4KICAgIDxyZWN0IGZpbGw9IiM0RTZGODIiIG1hc2s9InVybCgjYikiIHg9IjE4MCIgeT0iNTA2IiB3aWR0aD0iMTk0IiBoZWlnaHQ9IjYiIHJ4PSIzIi8+CiAgICA8cmVjdCBmaWxsPSIjNEU2RjgyIiBtYXNrPSJ1cmwoI2IpIiB4PSIxODAiIHk9IjUyMiIgd2lkdGg9IjIwNyIgaGVpZ2h0PSI2IiByeD0iMyIvPgogICAgPHRleHQgbWFzaz0idXJsKCNiKSIgZm9udC1mYW1pbHk9Ik51bml0by1CbGFjaywgTnVuaXRvIiBmb250LXNpemU9IjE0NCIgZm9udC13ZWlnaHQ9IjcwMCIgZmlsbD0iI0ZGMzk3MyI+CiAgICAgIDx0c3BhbiB4PSIxNjAiIHk9IjM0OSI+NDA0PC90c3Bhbj4KICAgIDwvdGV4dD4KICAgIDx0ZXh0IG1hc2s9InVybCgjYikiIGZvbnQtZmFtaWx5PSJPcGVuU2Fucy1TZW1pYm9sZCwgT3BlbiBTYW5zIiBmb250LXNpemU9IjE4IiBmb250LXdlaWdodD0iNTAwIiBmaWxsPSIjNEU2RjgyIj4KICAgICAgPHRzcGFuIHg9IjE2MCIgeT0iMjI3Ij5SRVBPUlQ8L3RzcGFuPgogICAgPC90ZXh0PgogIDwvZz4KPC9zdmc+Cg=='
+        },
+        'subdir/img.jpg': {
+          path: 'img/subdir/img.jpg',
+          ext: '.jpg',
+          width: 1200,
+          widthPx: '1200px',
+          height: 630,
+          heightPx: '630px',
+          aspectRatio: 0.525,
+          hash: '741d27da'
+        }
+      };
+      `
     },
     {
       title: 'Snapshot',
--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const nodePath = require('path')
+const fs = require('fs')
 const glob = require('glob-fs')()
 const imageSize = require('image-size')
 const pluginName = 'babel-plugin-image-size'
+const crypto = require('crypto')
+
+const md5File = (content) => {
+  const hash = crypto.createHash('md5')
+  hash.update(content)
+  return hash.digest('hex')
+}
 
 module.exports = function(babel) {
   const t = babel.types
@@ -10,6 +18,8 @@ module.exports = function(babel) {
       StringLiteral: function StringLiteral(path, state) {
         if (path.node.value !== '__babelPluginImageHelper__') return
 
+        const limit = state.opts.limit || 10000 // Byte limit to inline files as Data URL
+        const hashLength = state.opts.hashLength || 8
         const imgDir = nodePath.join('.', state.opts.path)
         let files = glob.readdirSync(nodePath.join(imgDir, '**/*.{jpg,jpeg,png,gif,svg}'))
 
@@ -26,37 +36,59 @@ module.exports = function(babel) {
             return
           }
 
+          const imgData = fs.readFileSync(imgPath, 'base64')
+          const stats = fs.statSync(imgPath)
+          let md5sum = md5File(imgData)
+          md5sum = md5sum.substr(md5sum.length - hashLength, hashLength)
+
           const imgAbsPath = nodePath.join('.', imgPath)
           const key = nodePath.relative(imgDir, imgAbsPath)
 
+          const properties = [
+            t.objectProperty(
+              t.identifier('path'),
+              t.stringLiteral(imgPath),
+            ),
+            t.objectProperty(
+              t.identifier('ext'),
+              t.stringLiteral(nodePath.extname(imgPath).toLowerCase()),
+            ),
+            t.objectProperty(
+              t.identifier('width'),
+              t.numericLiteral(dimensions.width),
+            ),
+            t.objectProperty(
+              t.identifier('widthPx'),
+              t.stringLiteral(`${dimensions.width}px`),
+            ),
+            t.objectProperty(
+              t.identifier('height'),
+              t.numericLiteral(dimensions.height),
+            ),
+            t.objectProperty(
+              t.identifier('heightPx'),
+              t.stringLiteral(`${dimensions.height}px`),
+            ),
+            t.objectProperty(
+              t.identifier('aspectRatio'),
+              t.numericLiteral(dimensions.height / dimensions.width),
+            ),
+            t.objectProperty(
+              t.identifier('hash'),
+              t.stringLiteral(md5sum),
+            ),
+          ]
+
+          if (stats.size <= limit) {
+            properties.push(t.objectProperty(
+              t.identifier('data'),
+              t.stringLiteral(imgData),
+            ))
+          }
+
           return t.objectProperty(
             t.identifier(`'${key}'`),
-            t.objectExpression([
-              t.objectProperty(
-                t.identifier('path'),
-                t.stringLiteral(imgPath),
-              ),
-              t.objectProperty(
-                t.identifier('width'),
-                t.numericLiteral(dimensions.width),
-              ),
-              t.objectProperty(
-                t.identifier('widthPx'),
-                t.stringLiteral(`${dimensions.width}px`),
-              ),
-              t.objectProperty(
-                t.identifier('height'),
-                t.numericLiteral(dimensions.height),
-              ),
-              t.objectProperty(
-                t.identifier('heightPx'),
-                t.stringLiteral(`${dimensions.height}px`),
-              ),
-              t.objectProperty(
-                t.identifier('aspectRatio'),
-                t.numericLiteral(dimensions.height / dimensions.width),
-              ),
-            ])
+            t.objectExpression(properties)
           )
         })
 
