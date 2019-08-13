ad5846e890218eb316faeb7bf489cfb3a4512183
Erik Fossvold <erik.fossvold@fanbooster.com>
2019-08-13 16:26:56 +0200

    Added support for httpPath. Added option for selecting which properties are added to the images.

--- index.js
+++ index.js
@@ -11,8 +11,11 @@ const md5File = (content) => {
   return hash.digest('hex')
 }
 
+const IMG_PROPS = ['src', 'absPath', 'ext', 'width', 'widthPx', 'height', 'heightPx', 'aspectRatio', 'hash', 'data']
+
 module.exports = function(babel) {
   const t = babel.types
+  let x = 1
   return {
     visitor: {
       StringLiteral: function StringLiteral(path, state) {
@@ -21,6 +24,18 @@ module.exports = function(babel) {
         const limit = state.opts.limit || 10000 // Byte limit to inline files as Data URL
         const hashLength = state.opts.hashLength || 8
         const imgDir = state.opts.path
+        const imgProps = state.opts.props || IMG_PROPS
+
+        imgProps.forEach(prop => {
+          if (!IMG_PROPS.includes(prop)) {
+            throw(`Property '${prop}' is invalid. Valid properties are ${IMG_PROPS.join(', ')}`)
+          }
+        })
+
+        console.log({
+          imgDir
+        })
+
         let files = glob.sync(nodePath.join(imgDir, '**/*.{jpg,jpeg,png,gif,svg}'))
 
         const nodes = files.map(imgPath => {
@@ -33,50 +48,107 @@ module.exports = function(babel) {
             return
           }
 
+          const key = nodePath.relative(imgDir, imgPath)
+          const imgHTTPPath = nodePath.join(state.opts.httpPath ||  '', key)
           const imgData = fs.readFileSync(imgPath, 'base64')
           const stats = fs.statSync(imgPath)
           let md5sum = md5File(imgData)
           md5sum = md5sum.substr(md5sum.length - hashLength, hashLength)
 
-          const imgAbsPath = nodePath.join('.', imgPath)
-          const key = nodePath.relative(imgDir, imgAbsPath)
+          if (x == 1) {
+            console.log({
+              key,
+              imgDir,
+              imgPath,
+              imgHTTPPath,
+            })
+            x = 2
+          }
 
-          const properties = [
-            t.objectProperty(
-              t.identifier('path'),
-              t.stringLiteral(imgPath),
-            ),
-            t.objectProperty(
-              t.identifier('ext'),
-              t.stringLiteral(nodePath.extname(imgPath).toLowerCase()),
-            ),
-            t.objectProperty(
-              t.identifier('width'),
-              t.numericLiteral(dimensions.width),
-            ),
-            t.objectProperty(
-              t.identifier('widthPx'),
-              t.stringLiteral(`${dimensions.width}px`),
-            ),
-            t.objectProperty(
-              t.identifier('height'),
-              t.numericLiteral(dimensions.height),
-            ),
-            t.objectProperty(
-              t.identifier('heightPx'),
-              t.stringLiteral(`${dimensions.height}px`),
-            ),
-            t.objectProperty(
-              t.identifier('aspectRatio'),
-              t.numericLiteral(dimensions.height / dimensions.width),
-            ),
-            t.objectProperty(
-              t.identifier('hash'),
-              t.stringLiteral(md5sum),
-            ),
-          ]
+          const properties = []
 
-          if (stats.size <= limit) {
+          if (imgProps.includes('absPath')) {
+            properties.push(
+              t.objectProperty(
+                t.identifier('path'),
+                t.stringLiteral(imgPath),
+              )
+            )
+          }
+
+          if (imgProps.includes('ext')) {
+            properties.push(
+              t.objectProperty(
+                t.identifier('ext'),
+                t.stringLiteral(nodePath.extname(imgPath).toLowerCase()),
+              )
+            )
+          }
+
+          if (imgProps.includes('src')) {
+            properties.push(
+              t.objectProperty(
+                t.identifier('src'),
+                t.stringLiteral(imgHTTPPath),
+              )
+            )
+          }
+
+          if (imgProps.includes('width')) {
+            properties.push(
+              t.objectProperty(
+                t.identifier('width'),
+                t.numericLiteral(dimensions.width),
+              )
+            )
+          }
+
+          if (imgProps.includes('widthPx')) {
+            properties.push(
+              t.objectProperty(
+                t.identifier('widthPx'),
+                t.stringLiteral(`${dimensions.width}px`),
+              )
+            )
+          }
+
+          if (imgProps.includes('height')) {
+            properties.push(
+              t.objectProperty(
+                t.identifier('height'),
+                t.numericLiteral(dimensions.height),
+              )
+            )
+          }
+
+          if (imgProps.includes('heightPx')) {
+            properties.push(
+              t.objectProperty(
+                t.identifier('heightPx'),
+                t.stringLiteral(`${dimensions.height}px`),
+              )
+            )
+          }
+
+          if (imgProps.includes('aspectRatio')) {
+            properties.push(
+              t.objectProperty(
+                t.identifier('aspectRatio'),
+                t.numericLiteral(dimensions.height / dimensions.width),
+              )
+            )
+          }
+
+          if (imgProps.includes('hash')) {
+            properties.push(
+              t.objectProperty(
+                t.identifier('hash'),
+                t.stringLiteral(md5sum),
+              )
+            )
+          }
+
+          if (imgProps.includes('data') && stats.size <= limit) {
             properties.push(t.objectProperty(
               t.identifier('data'),
               t.stringLiteral(imgData),
--- run.js
+++ run.js
@@ -1,4 +1,5 @@
 // run.js
+const path = require('path')
 const babel = require('@babel/core')
 const fs = require('fs')
 const plugin = require('./index.js')
@@ -8,7 +9,11 @@ const fileName = process.argv[2] || 'example.js'
 if (!fs.existsSync(fileName)) throw Error(`File '${fileName}' not found`)
 
 const out = babel.transformFileSync(fileName, {
-  plugins: [[plugin, {path: './img'}]],
+  plugins: [[plugin, {
+    path: path.join(__dirname, '/img'),
+    httpPath: '/static',
+    // props: ['httpPath', 'width', 'height', 'hash'],
+  }]],
   babelrc: false // So we don't get babelrc from whole project
 }).code
 
